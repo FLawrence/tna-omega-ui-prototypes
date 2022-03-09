7e6df9a934670a66c0c3e88ddb1936cd68ca5853
Nick Lee <nick@nicklee.co.uk>
2022-03-09 13:49:19 +0000

    New conditional field for access

--- tnaomegauiprototypes/static_src/javascript/main.js
+++ tnaomegauiprototypes/static_src/javascript/main.js
@@ -4,6 +4,7 @@ import BulkSelectAll from './components/bulk-select-all';
 import BulkEdit from './components/bulk-edit';
 import BulkAction from './components/bulk-action';
 import StagesRedirect from './components/stages-redirect';
+import ConditionalField from './components/conditional-field';
 
 import './components/autocomplete';
 
@@ -24,6 +25,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     for (const bulkaction of document.querySelectorAll(BulkAction.selector())) {
         new BulkAction(bulkaction);
+    }
+
+    for (const conditionalfield of document.querySelectorAll(
+        ConditionalField.selector(),
+    )) {
+        new ConditionalField(conditionalfield);
     }
 
     for (const stagesredirect of document.querySelectorAll(
